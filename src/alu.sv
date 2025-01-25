637bf0ef01a3591af733f99b5e265841810f56b7
JaYveC <yves@clercnet.ch>
2025-01-25 18:39:49 +0000

    make alu 16 bits

--- src/alu.sv
+++ src/alu.sv
@@ -1,14 +1,14 @@
 module alu (
   input logic c_in,
-  input logic [7:0] a_in, b_in,
+  input logic [15:0] a_in, b_in,
   input logic [3:0] alu_sel,
   output logic c_out,
-  output logic [7:0] out
+  output logic [15:0] out
 );
 
   always_comb begin
     case (alu_sel)
-      4'b0 : {c_out,out} = a_in + b_in + {7'b0,c_in};
+      4'b0 : {c_out,out} = a_in + b_in + {15'b0,c_in};
       default: out = a_in | b_in;
     endcase
   end
--- src/alu_tb.sv
+++ src/alu_tb.sv
@@ -1,10 +1,10 @@
 module alu_tb ();
 reg c_in;
-reg [7:0] a_in, b_in;
+reg [15:0] a_in, b_in;
 reg [3:0] alu_sel;
 /* verilator lint_off UNUSEDSIGNAL */
 wire c_out;
-wire [7:0] out;
+wire [15:0] out;
 /* verilator lint_off UNUSEDSIGNAL */
 
 alu dut (
@@ -19,23 +19,23 @@ alu dut (
 initial begin
    $dumpfile("alu_tb.vcd");
    $dumpvars(0, alu_tb);
-   a_in = 8'b1001;
-   b_in = 8'b111;
+   a_in = 16'b1001;
+   b_in = 16'b111;
    c_in = 1'b1;
    alu_sel = 4'b0;
    #20
-   a_in = 8'b01011;
-   b_in = 8'b110;
+   a_in = 16'b01011;
+   b_in = 16'b110;
    c_in = 1'b1;
    alu_sel = 4'b1;
    #20
-   a_in = 8'b1101;
-   b_in = 8'b101;
+   a_in = 16'b1101;
+   b_in = 16'b101;
    c_in = 1'b0;
    alu_sel = 4'b0;
    #20
-   a_in = 8'b11111111;
-   b_in = 8'b11111111;
+   a_in = 16'b1111111111111111;
+   b_in = 16'b1111111111111111;
    c_in = 1'b1;
    alu_sel = 4'b0;
    #20
