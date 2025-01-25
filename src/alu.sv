180533abc759afe12a031a3d0c3e4b026a47cda3
JaYveC <yves@clercnet.ch>
2025-01-25 22:00:13 +0000

    add isa sheet and add more alu functions

--- src/alu.sv
+++ src/alu.sv
@@ -8,7 +8,19 @@ module alu (
 
   always_comb begin
     case (alu_sel)
-      4'b0 : {c_out,out} = a_in + b_in + {15'b0,c_in};
+      4'b0000 : {c_out,out} = a_in + b_in + {15'b0,c_in};
+      4'b0001 : {c_out,out} = a_in - b_in + {15'b0,c_in};
+      4'b0010 : out = a_in | b_in;
+      4'b0011 : out = a_in & b_in;
+      4'b0100 : out = a_in ^ b_in;
+      4'b0101 : out = a_in ~| b_in;
+      4'b0110 : out = a_in ~& b_in;
+      4'b0111 : out = a_in << b_in;
+      4'b1000 : out = a_in >> b_in;
+      4'b1001 : out = a_in <<< b_in;
+      4'b1010 : out = a_in >>> b_in;
+      4'b1011 : out = a_in <<< b_in;
+      4'b1100 : out = ~a_in;
       default: out = a_in | b_in;
     endcase
   end
