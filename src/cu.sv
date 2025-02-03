118e6e49b62b46e131fd5b2517f5a139764207f8
JaYveC <yves@clercnet.ch>
2025-02-03 19:04:05 +0000

    add code for LDIM instruction

---
+++ src/cu.sv
@@ -0,0 +1,26 @@
+module cu (
+    input logic [5:0] opcode,
+    output logic alu_c_in, alu_enable, reg_read_a, reg_read_b, reg_write, reg_reset,
+    output logic [1:0] inst_type,
+    output logic [2:0] wb_sel,
+    output logic [3:0] alu_sel
+);
+    always @(opcode) begin
+    case (opcode)
+        6'b001110 : begin     //LDIM
+            inst_type <= 2'b01;
+            alu_c_in <= 0; alu_enable <= 0;
+            alu_sel <= 4'b0000;
+            reg_read_a <= 0; reg_read_b <= 0; reg_write <= 1; reg_reset <= 0;
+            wb_sel <= 3'b001;
+        end
+        default : begin     //NOP
+            inst_type <= 2'b00;
+            alu_c_in <= 0; alu_enable <= 0;
+            alu_sel <= 4'b0000;
+            reg_read_a <= 0; reg_read_b <= 0; reg_write <= 0; reg_reset <= 0;
+            wb_sel <= 3'b000;
+        end
+    endcase
+  end
+endmodule
